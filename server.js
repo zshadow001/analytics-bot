6248bbb48d3e76a35393130b483b572449e55951
zshadow001 <parmarprince1982010@gmail.com>
2026-08-08 17:56:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 
-const BOT_TOKEN = "8975100574:AAGHgoUJQrQVQy7FCb1fpqhiZ-DHEUrwu6k";
+const BOT_TOKEN = os.getenv("BOT_TOKEN");
 const DB_FILE = "./database.json";
 
 // ===== DB =====
